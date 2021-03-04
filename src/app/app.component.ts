fb8e0a02c91bf429643e030e8b766b011e9755b8
ademalkan <43451577+ademalkan@users.noreply.github.com>
2021-03-04 19:20:25 +0300

    download files

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'twitter-login-page';
+  name: String = "";
+  password: String = "";
+  model: AppComponent = <AppComponent>{}
+  ngOnInit(): void {
+  }
+}
